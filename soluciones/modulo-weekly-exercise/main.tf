04cc7faaa2b4f98211add3c014861f6aefbfa43b
ploopez <166410013+ploopez@users.noreply.github.com>
2024-05-11 13:50:42 +0200

    Update main.tf

--- soluciones/modulo-weekly-exercise/main.tf
+++ soluciones/modulo-weekly-exercise/main.tf
@@ -1,14 +1,11 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "3.102.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
 }
 
 resource "azurerm_virtual_network" "vnet" {
